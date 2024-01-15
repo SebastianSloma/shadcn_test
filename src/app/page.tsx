e1259b3a8b53194b624299932c4b07bb8367480d
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-15 17:51:36 +0100

    added calendar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+import React,{useState} from 'react';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
 
 const page = () => {
-  return (
-    <div>
-      <h1>Button Page</h1>
-      <Button variant='ghost'>Click me</Button>
-      <Button variant='destructive'>Click me</Button>
-      </div>
-  )
-}
 
-export default page
+	return (
+		<div>
+			<h1>Button Page</h1>
+			<Button variant='ghost'>Click me</Button>
+			<Button variant='destructive'>Click me</Button>
+			<Calendar
+				mode='single'
+			
+				className='rounded-md border'
+			/>
+		</div>
+	);
+};
+
+export default page;
