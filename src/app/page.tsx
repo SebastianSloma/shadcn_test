5d5f86d3af20caaa19f26a9f27abe72505c70c22
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-18 21:09:35 +0100

    ToastWithAction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,22 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
 import DrawerShadcn from '@/components/DrawerShadcn';
-import { ToastSimple } from '@/components/Toastt';
+import { ToastWithAction } from '@/components/ToastWithAction';
 import { Acordion } from '@/components/Acordion';
 
-
-
 const page = () => {
-
 	return (
 		<div>
 			<h1>Button Page</h1>
 			<Button variant='ghost'>Click me</Button>
 			<Button variant='destructive'>Click me</Button>
-			<Calendar
-				mode='single'
-			
-				className='rounded-md border'
-			/>
-			<DrawerShadcn/>
+			<Calendar mode='single' className='rounded-md border' />
+			<DrawerShadcn />
 			<div>
-				<ToastSimple/>
+				<ToastWithAction />
 			</div>
 			<div>
-				<Acordion/>
+				<Acordion />
 			</div>
 		</div>
 	);
